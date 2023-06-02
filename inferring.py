602cc529ac1598bb2b64e65ffbb9b4824164e711
Chris Mann <chrispmann@gmail.com>
2023-06-02 13:40:49 -0700

    initial commit

---
+++ inferring.py
@@ -0,0 +1,40 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-4"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+lamp_review = """
+This is the worst lamp ever. It burned down my house and killed my family. Lamplight should be ashamed of themselves.
+"""
+
+prompt = f"""
+Identify the following items from the review text: 
+- Sentiment (positive or negative)
+- Is the reviewer expressing anger? (true or false)
+- Item purchased by reviewer
+- Company that made the item
+
+The review is delimited with triple backticks. \
+Format your response as a JSON object with \
+"Sentiment", "Anger", "Item" and "Brand" as the keys.
+If the information isn't present, use "unknown" \
+as the value.
+Make your response as short as possible.
+Format the Anger value as a boolean.
+
+Review text: '''{lamp_review}'''
+"""
+response = get_completion(prompt)
+print(response)

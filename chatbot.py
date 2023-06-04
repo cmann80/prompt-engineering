c2a7ef44ea6095baab979c434defa801c2eb6cf7
Chris Mann <chrispmann@gmail.com>
2023-06-04 11:54:00 -0700

    simplified chatbot, still doesn't work though it does in the online Jupyter notebook

--- chatbot.py
+++ chatbot.py
@@ -3,23 +3,14 @@
 import os
 import openai
 from dotenv import load_dotenv, find_dotenv
-import panel as pn
-pn.extension()
+
 
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
-def get_completion(prompt, model="gpt-4"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
 
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+def get_completion_from_messages(messages, model="gpt-4", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -27,37 +18,6 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
     )
 #     print(str(response.choices[0].message))
     return response.choices[0].message["content"]
-
-# messages =  [  
-# {'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
-# {'role':'user', 'content':'tell me a joke'},   
-# {'role':'assistant', 'content':'Why did the chicken cross the road'},   
-# {'role':'user', 'content':'I don\'t know'}  ]
-
-# response = get_completion_from_messages(messages, temperature=1)
-# print(response)
-
-# messages =  [  
-# {'role':'system', 'content':'You are friendly chatbot.'},    
-# {'role':'user', 'content':'Hi, my name is Isa'}  ]
-# response = get_completion_from_messages(messages, temperature=1)
-# print(response)
-
-# messages =  [  
-# {'role':'system', 'content':'You are friendly chatbot.'},    
-# {'role':'user', 'content':'Yes,  can you remind me, What is my name?'}  ]
-# response = get_completion_from_messages(messages, temperature=1)
-# print(response)
-
-# messages =  [  
-# {'role':'system', 'content':'You are friendly chatbot.'},
-# {'role':'user', 'content':'Hi, my name is Isa'},
-# {'role':'assistant', 'content': "Hi Isa! It's nice to meet you. \
-# Is there anything I can help you with today?"},
-# {'role':'user', 'content':'Yes, you can remind me, What is my name?'}  ]
-# response = get_completion_from_messages(messages, temperature=1)
-# print(response)
-
 
 
 def collect_messages(_):
@@ -69,9 +29,12 @@ def collect_messages(_):
     panels.append(
         pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
     panels.append(
-        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'})))
+        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, styles={'background-color': '#F6F6F6'})))
 
     return pn.Column(*panels)
+
+import panel as pn
+pn.extension()
 
 panels = []
 
@@ -116,6 +79,16 @@ dashboard = pn.Column(
     pn.Row(button_conversation),
     pn.panel(interactive_conversation, loading_indicator=True, height=300),
 )
+
+messages =  context.copy()
+messages.append(
+{'role':'system', 'content':'create a json summary of the previous food order. Itemize the price for each item\
+    The fields should be 1) pizza, include size 2) list of toppings 3) list of drinks, include size   4) list of sides include size  5)total price '},    
+)
+    #The fields should be 1) pizza, price 2) list of toppings 3) list of drinks, include size include price  4) list of sides include size include price, 5)total price '},    
+
+response = get_completion_from_messages(messages, temperature=0)
+print(response)
 
 dashboard
 # %%
